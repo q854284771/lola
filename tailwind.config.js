cffa656b7241e04e7f879f889a5d93ef8c566f83
q854284771 <34053464+q854284771@users.noreply.github.com>
2022-07-31 15:37:05 +0800

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  darkMode: 'class', // or 'media'
+  minify: true,
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx,css,md,mdx,html,json,scss}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
